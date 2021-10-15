ca5b452507d78f9f18a87f797fb269e17c537b11
Alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2021-10-15 21:11:56 +0300

    2.1 Разметка страницы регистрации

---
+++ src/views/Register.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{name: 'home'}"> Need an account? </router-link>
+          </p>
+          VALIDATION ERRORS
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Username"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right" type="submit">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'McvRegister',
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {
+    onSubmit() {
+      console.log('onSubmit Register');
+    },
+  },
+};
+</script>
