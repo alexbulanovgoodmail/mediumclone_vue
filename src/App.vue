12e02aed589fec3935a9ca7b6ea5e44216215221
Alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2021-10-17 19:40:35 +0300

    3.3 Создаем верхнее меню

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
-  <div>Hello Medium Clone</div>
+  <mcv-top-bar />
+
   <router-view />
 </template>
+
+<script>
+import McvTopBar from '@/components/TopBar';
+export default {
+  name: 'McvApp',
+  components: {
+    McvTopBar,
+  },
+};
+</script>
